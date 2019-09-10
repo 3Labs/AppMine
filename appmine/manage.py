fa68aac46ecad1667610f8cbbd89e2b8eb682004
3Labs <tri3labs@gmail.com>
2019-09-11 03:33:19 +0700

    Update manage.py

--- appmine/manage.py
+++ appmine/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "financialchat.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appmine.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
